6d4a65740ec03e04e904390b5b4393daf16f1676
Simtech1976 <simtech.uni@outlook.com>
2026-02-06 16:49:32 +0000

    Add initial variable definitions for name, age, height, human status, and months

---
+++ 01 Variables.py
@@ -0,0 +1,6 @@
+str_name = 'Simon'
+int_age = 49
+float_height = 5.10
+bool_human = True
+list_months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+
