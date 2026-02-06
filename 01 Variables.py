278af76d923bec445b019e629af4f8b32cee32be
Simtech1976 <simtech.uni@outlook.com>
2026-02-06 17:01:17 +0000

    latest

--- 01 Variables.py
+++ 01 Variables.py
@@ -4,3 +4,28 @@ float_height = 5.10
 bool_human = True
 list_months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
 
+""" Math operations with variables """
+
+addition = 5 + 4
+subtraction = 5 - 4
+multiplication = 5 * 4
+division = 5 / 4
+floor_division = 5 // 4
+modulus = 5 % 4
+exponentiation = 5 ** 4
+
+print('-' * 50)
+print('Basic math operations')
+print('-' * 50)
+print('Addition:' + str(addition))
+print('Subtraction:' + str(subtraction))
+print('Multiplication:' + str(multiplication))
+print('Division:' + str(division))
+print('Floor Division:' + str(floor_division))
+print('Modulus:' + str(modulus))
+print('Exponentiation:' + str(exponentiation))
+print('_' * 50)
+
+var1 = 5
+var2 = 10
+
